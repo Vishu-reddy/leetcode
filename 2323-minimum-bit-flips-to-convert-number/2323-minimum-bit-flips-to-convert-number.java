746c146a56c8955ae605c2eb3fdeaf0e8c501aee
sadu vishwanath <153756418+Vishu-reddy@users.noreply.github.com>
2024-09-11 06:31:14 +0530

    Time: 0 ms (100%), Space: 41.2 MB (5.49%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,11 +1,22 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int ans=start^goal;
+        int[] s=decimalToBinary(start);
+        int[] g=decimalToBinary(goal);
         int count=0;
-        while(ans!=0){
-            count++;
-            ans=(ans&(ans-1));
+        for(int i=0;i<s.length;i++){
+            if(s[i]!=g[i]){
+                count++;
+            }
         }
         return count;
+    }
+    public int[] decimalToBinary(int num){
+        int[] binary = new int[35];
+        int id = 0;
+        while (num > 0) {
+            binary[id++] = num % 2;
+            num = num / 2;
+        }
+        return binary;
     }
 }
