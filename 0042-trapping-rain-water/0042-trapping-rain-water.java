e85c83e988ef27011017cb768d1b7ed07baf1709
sadu vishwanath <153756418+Vishu-reddy@users.noreply.github.com>
2024-09-12 10:50:44 +0530

    Time: 1 ms (67.23%), Space: 46.5 MB (19.36%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -27,6 +27,35 @@
 //         return res;
 //     }
 // }
+// class Solution {
+//     public int trap(int[] height) {
+//         int l=0;
+//         int r=height.length-1;
+//         int ml=height[l];
+//         int mr=height[r];
+//         int res=0;
+//         while(l<r){
+//             if(height[l]<height[r]){
+//                 l++;
+//                 int w=ml-height[l];
+//                 if(w>0){
+//                     res+=w;
+//                 }
+//                 ml=Math.max(ml,height[l]);
+//             }
+//             else{
+//                 r--;
+//                 int w=mr-height[r];
+//                 if(w>0){
+//                     res+=w;
+//                 }
+//                 mr=Math.max(mr,height[r]);
+//             }
+
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public int trap(int[] height) {
         int l=0;
@@ -51,7 +80,6 @@ class Solution {
                 }
                 mr=Math.max(mr,height[r]);
             }
-
         }
         return res;
     }
