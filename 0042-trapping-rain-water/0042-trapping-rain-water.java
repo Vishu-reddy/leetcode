d69ce852e5d6d97b3bc180f20ad2ca71ba61f286
sadu vishwanath <153756418+Vishu-reddy@users.noreply.github.com>
2024-09-12 11:06:47 +0530

    Time: 0 ms (100%), Space: 46.4 MB (28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -58,6 +58,9 @@
 // }
 class Solution {
     public int trap(int[] height) {
+        if(height.length==0){
+            return 0;
+        }
         int l=0;
         int r=height.length-1;
         int ml=height[l];
