28be548047d08b12a18dccf7c501881967a04b76
sadu vishwanath <153756418+Vishu-reddy@users.noreply.github.com>
2024-09-17 15:36:35 +0530

    Time: 3 ms (26.94%), Space: 47.3 MB (20.21%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,44 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int l=0;
-        int r=nums.length-1;
-        while(l<r){
-            if(nums[l]+nums[r]==target){
-                return new int[]{l+1,r+1};
-            }
-            if(nums[l]+nums[r]<target){
-                l++;
-            }
-            if(nums[l]+nums[r]>target){
-                r--;
-            }
+       int l=0;
+       int r=nums.length-1;
+       while(l<r){
+        if(nums[l]+nums[r]==target){
+            return new int[] {l+1,r+1};
         }
-        return new int[]{};
+        if(nums[l]+nums[r]<target){
+            l++;
+        }
+        if(nums[l]+nums[r]>target){
+            r--;
+        }
+       }
+        return new int[] {};
+
+
+
+
+
+
+
+
+
+
+
+
+        // int l=0;
+        // int r=nums.length-1;
+        // while(l<r){
+        //     if(nums[l]+nums[r]==target){
+        //         return new int[]{l+1,r+1};
+        //     }
+        //     if(nums[l]+nums[r]<target){
+        //         l++;
+        //     }
+        //     if(nums[l]+nums[r]>target){
+        //         r--;
+        //     }
+        // }
+        // return new int[]{};
     }
 }
