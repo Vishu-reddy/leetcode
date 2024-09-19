0346f3214fcad1221694fac20693c3d04beb4825
sadu vishwanath <153756418+Vishu-reddy@users.noreply.github.com>
2024-09-19 06:17:27 +0530

    Time: 5 ms (9.45%), Space: 42.4 MB (60.69%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    private Map<String, List<Integer>> memo = new HashMap<>();
+
+    public List<Integer> diffWaysToCompute(String expression) {
+        if (memo.containsKey(expression)) {
+            return memo.get(expression);
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        if (expression.matches("-?\\d+")) { // Check if expression is a number
+            result.add(Integer.parseInt(expression));
+            return result;
+        }
+
+        for (int i = 0; i < expression.length(); i++) {
+            char op = expression.charAt(i);
+            if (op == '+' || op == '-' || op == '*') {
+                String leftExpr = expression.substring(0, i);
+                String rightExpr = expression.substring(i + 1);
+
+                List<Integer> leftResults = diffWaysToCompute(leftExpr);
+                List<Integer> rightResults = diffWaysToCompute(rightExpr);
+
+                for (int left : leftResults) {
+                    for (int right : rightResults) {
+                        if (op == '+') result.add(left + right);
+                        else if (op == '-') result.add(left - right);
+                        else if (op == '*') result.add(left * right);
+                    }
+                }
+            }
+        }
+
+        memo.put(expression, result);
+        return result;
+    }
+}
